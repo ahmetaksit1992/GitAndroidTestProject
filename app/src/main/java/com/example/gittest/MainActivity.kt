764e2ca1b50c68df1d512f3569e9aa0f87dfeff7
ahmet aksit <ahmetsami.aksit@aurorabilisim.com>
2021-04-12 09:34:22 +0300

    Inital Commit

--- app/src/main/java/com/example/gittest/MainActivity.kt
+++ app/src/main/java/com/example/gittest/MainActivity.kt
@@ -9,3 +9,12 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 }
+
+//todo 6:15 of video explains how to do stuff on repository --> dont have
+//todo after having access to repo (github) VCS -> Git -> Push
+//todo master -> define remote (URL to your repo)
+
+//todo File -> new -> project from version control -> URL (put UltimateAndroidReference repo link)
+
+//todo MERGE:
+//todo 
